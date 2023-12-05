490c92f057724ea7e328aa2fa5e0056c9403dfc0
Artem Chudan(Chudak-byte) <artem.chudan@icloud.com>
2023-12-05 16:08:38 +0200

    Udemy lesson 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 
 // const numberOfFilms = +prompt("Яку кількість фільмів ви переглянули?", "");
 // const personalMovieDB = {
@@ -17,16 +17,31 @@
 // personalMovieDB.movies[a] = b;
 // personalMovieDB.movies[c] = d;
 // console.log(personalMovieDB);
-const num = 50;
+// const num = 50;
 
-switch (num) {
-  case 120:
-    console.log("bad");
-    break;
-  case 51:
-    console.log("good");
-    break;
-  default:
-    console.log("null");
-    break;
+// switch (num) {
+//   case 120:
+//     console.log("bad");
+//     break;
+//   case 51:
+//     console.log("good");
+//     break;
+//   default:
+//     console.log("null");
+//     break;
+// }
+// let num = 45;
+
+// do {
+//   console.log(num);
+//   num++;
+// }
+// while (num <= 55);
+
+for (let i = 1; i <= 12; i++) {
+  if (i === 9) {
+    continue;
+  }
+  console.log(i);
+  
 }
