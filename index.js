5cc4856f4716a5d9e7594ad5934a304400281b82
Artem Chudan(Chudak-byte) <artem.chudan@icloud.com>
2023-12-06 23:37:08 +0200

    add new task

--- index.js
+++ index.js
@@ -38,10 +38,70 @@
 // }
 // while (num <= 55);
 
-for (let i = 1; i <= 12; i++) {
-  if (i === 9) {
-    continue;
-  }
-  console.log(i);
+// for (let i = 1; i <= 12; i++) {
+//   if (i === 9) {
+//     continue;
+//   }
+//   console.log(i);
+
+// }
+// for (let i = 0; i <= 3; i++) {
+//   console.log(i);
+//   for (let j = 0; j <= 3; j++) {
+//     console.log(j);
+//   }
+// }
+
+// let result = "";
+// const length = 4;
+
+//  first: for (let i = 1; i < length; i++) {
+//   console.log(`First level: ${i}`);
+//   for (let j = 0; j < i; j++) {
+//     console.log(`Second level: ${j}`);
+//     for (let x = 0; x < j; x++) {
+//       if(x === 2) break first;
+//       console.log(`Third level: ${x}`);
+//     }
+//   }
+
+// }
+// for (let i = 2; i <= 16; i++) {
+//   if (i % 2 === 0) {
+//       continue;
+//   } else {
+//       console.log(i);
+//   }
+// }
+// let i = 2;
+
+// while (i <= 16) {
+//   if (i % 2 === 0) {
+//     i++;
+//     continue;
+//   } else {
+//     console.log(i);
+//   }
+//   i++
+// }
+
+  // const arrayOfNumbers = [];
+  //   for (let i = 5; i <= 10; i++) {
+  //     console.log(i);
+  //     var total = arrayOfNumbers.push(i);
+  //   }
+  // return arrayOfNumbers
+  const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    for(let i = 0; i < arr.length; i++){
+      result[i] = arr[i];
+    }
+  console.log(result);
+    
+    // Не трогаем
+    return result;
   
-}
+  
+
+      
