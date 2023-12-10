89c96f60bd1e81ce841078bcf708345732cc181a
Chudak-byte <artem.chudan@icloud.com>
2023-12-10 15:07:58 +0200

    add task

--- index.js
+++ index.js
@@ -30,3 +30,15 @@
 //     console.log("null");
 //     break;
 // }
+
+let result = ' ';
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+  for (let j = 0; j < i; j++) {
+    result =+ "*";
+  }
+  result += "\n";
+  
+}
+console.log(result);
