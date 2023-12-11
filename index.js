08dd10106b28334ad4cfca518387eb50a6bd24de
Artem Chudan(Chudak-byte) <artem.chudan@icloud.com>
2023-12-11 16:17:59 +0200

    modefied

--- index.js
+++ index.js
@@ -130,19 +130,3 @@
 
 // console.log(result)
 // console.log("hello world");
-const numberOfFilms = +prompt("Яку кількість фільмів ви переглянули?", "");
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [1],
-  private: false,
-};
-
-for (let i = 0; i < 2; i++) {
-  const a = prompt("Один із останніх переглянутих фільмів", ""),
-    b = prompt("На скільки його оціните?", "");
-
-  personalMovieDB.movies[a] = b;
-}
-console.log(personalMovieDB);
--- pet-project.js
+++ pet-project.js
@@ -1,18 +1,17 @@
-
 "use srtict";
 const numberOfFilms = +prompt("Яку кількість фільмів ви переглянули?", "");
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
-  genres: [1],
+  genres: [],
   private: false,
 };
 
 for (let i = 0; i < 2; i++) {
   const a = prompt("Один із останніх переглянутих фільмів", ""),
-    b = prompt("На скільки його оціните?", "");
+        b = prompt("На скільки його оціните?", "");
 
-  personalMovieDB.movies[a] = b;
+        personalMovieDB.movies[a] = b;
 }
 console.log(personalMovieDB);
