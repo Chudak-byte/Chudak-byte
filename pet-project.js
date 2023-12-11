9b93f797e936e40e1879b1d1ba9b0470383a8be2
Chudak-byte <124993603+Chudak-byte@users.noreply.github.com>
2023-12-11 15:26:55 +0200

    Add files via upload

---
+++ pet-project.js
@@ -0,0 +1,19 @@
+"use srtict";
+const numberOfFilms = +prompt("Яку кількість фільмів ви переглянули?", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [1],
+  private: false,
+};
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один із останніх переглянутих фільмів", ""),
+    b = prompt("На скільки його оціните?", "");
+
+  personalMovieDB.movies[a] = b;
+}
+
+console.log(personalMovieDB);
+console.log("hello world");
