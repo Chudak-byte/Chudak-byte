11f1dfd4d4c002f4e8dbeb03db21cb6852a2689e
Artem Chudan(Chudak-byte) <artem.chudan@icloud.com>
2023-12-11 16:08:24 +0200

    test git repo

--- index.js
+++ index.js
@@ -130,3 +130,19 @@
 
 // console.log(result)
 // console.log("hello world");
+const numberOfFilms = +prompt("Яку кількість фільмів ви переглянули?", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [1],
+  private: false,
+};
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один із останніх переглянутих фільмів", ""),
+    b = prompt("На скільки його оціните?", "");
+
+  personalMovieDB.movies[a] = b;
+}
+console.log(personalMovieDB);
--- pet-project.js
+++ pet-project.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 "use srtict";
 const numberOfFilms = +prompt("Яку кількість фільмів ви переглянули?", "");
 const personalMovieDB = {
@@ -15,26 +15,4 @@ for (let i = 0; i < 2; i++) {
 
   personalMovieDB.movies[a] = b;
 }
-
 console.log(personalMovieDB);
-=======
-"use srtict";
-const numberOfFilms = +prompt("Яку кількість фільмів ви переглянули?", "");
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [1],
-  private: false,
-};
-
-for (let i = 0; i < 2; i++) {
-  const a = prompt("Один із останніх переглянутих фільмів", ""),
-    b = prompt("На скільки його оціните?", "");
-
-  personalMovieDB.movies[a] = b;
-}
-
-console.log(personalMovieDB);
->>>>>>> 9b93f797e936e40e1879b1d1ba9b0470383a8be2
-console.log("hello world");
