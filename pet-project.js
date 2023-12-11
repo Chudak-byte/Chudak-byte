aa2c9f1c80b0abd3ffc9de222a39f11bd5525708
Artem Chudan(Chudak-byte) <artem.chudan@icloud.com>
2023-12-11 15:20:49 +0200

    movies

--- pet-project.js
+++ pet-project.js
@@ -1,18 +1,18 @@
 "use srtict";
-// const numberOfFilms = +prompt("Яку кількість фільмів ви переглянули?", "");
-// const personalMovieDB = {
-// count: numberOfFilms,
-// movies: {},
-// actors: {},
-// genres: [],
-// private: false,
-// };
+const numberOfFilms = +prompt("Яку кількість фільмів ви переглянули?", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [1],
+  private: false,
+};
 
-// const a = prompt("Один із останніх переглянутих фільмів", ""),
-//   b = prompt("На скільки його оціните?", ""),
-//   c = prompt("Один із останніх переглянутих фільмів", ""),
-//   d = prompt("На скільки його оціните?", "");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один із останніх переглянутих фільмів", ""),
+    b = prompt("На скільки його оціните?", "");
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-// console.log(personalMovieDB);
+  personalMovieDB.movies[a] = b;
+}
+
+console.log(personalMovieDB.genres);
