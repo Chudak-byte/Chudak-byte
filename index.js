8ed7ae8e7015dcb1d18af1367efd6f6f16d06016
Artem Chudan(Chudak-byte) <artem.chudan@icloud.com>
2023-12-11 15:03:14 +0200

    add new tasks

--- index.js
+++ index.js
@@ -85,23 +85,47 @@
 //   i++
 // }
 
-  // const arrayOfNumbers = [];
-  //   for (let i = 5; i <= 10; i++) {
-  //     console.log(i);
-  //     var total = arrayOfNumbers.push(i);
-  //   }
-  // return arrayOfNumbers
-  const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
+// const arrayOfNumbers = [];
+//   for (let i = 5; i <= 10; i++) {
+//     console.log(i);
+//     var total = arrayOfNumbers.push(i);
+//   }
+// return arrayOfNumbers
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+//   const result = [];
 
-    for(let i = 0; i < arr.length; i++){
-      result[i] = arr[i];
+//   for(let i = 0; i < arr.length; i++){
+//     result[i] = arr[i];
+//   }
+// console.log(result);
+
+//   // Не трогаем
+//   return result;
+
+//   for (let i = 0; i <= 6; i++) {  //Самий скорочений варіант
+//     console.log('*'.repeat(i));
+// };
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [];
+
+// const reversed = data.reverse();
+// for(let i = 0; i < data.length; i++){
+//   result[i] = reversed[i];
+// }
+
+// console.log(result);
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) { // Умова рядок i має бути менше дорівнює 5
+    for (let j = 0; j < lines - i; j++) { // Умова рядок j має бути меншим чим 5 - i
+        result += " ";
     }
-  console.log(result);
-    
-    // Не трогаем
-    return result;
-  
-  
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
+}
 
-      
+console.log(result)
