806ee257ac1854cf9365c22d974a0e016e85487f
Chudak-byte <artem.chudan@icloud.com>
2023-12-12 12:05:04 +0200

    dell project test

--- index.js
+++ index.js
@@ -30,15 +30,33 @@
 //     console.log("null");
 //     break;
 // }
+// Зробити
+// let result = '';
+// const length = 7;
 
-let result = ' ';
-const length = 7;
-
-for (let i = 1; i < length; i++) {
-  for (let j = 0; j < i; j++) {
-    result =+ "*";
-  }
-  result += "\n";
+// for (let i = 1; i < length; i++) {
+//   for (let j = 0; j < i; j++) {
+//     result += "*";
+    
+//   }
   
-}
-console.log(result);
+
+//   result += "\n";
+  
+// }
+// console.log(result);
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+//     // Пишем решение вот тут
+//     for(let i = 0; i < data.length; i++){
+//         if (typeof(data[i]) === "number") {
+//             data[i] = data[i] * 2;
+//         } else if(typeof(data[i]) === "string") {
+//             data[i] = `${data[i]} - done`;
+//         }
+//     }
+//     console.log(data);
+    
+//     // Не трогаем
+//     return data;
